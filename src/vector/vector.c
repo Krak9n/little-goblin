721c69b35db9beb1678cca5cd29624a35177b735
eto <olhnts12@proton.me>
2026-08-20 15:45:29 +0200

    Implemented a bunch of things, needs polishing. Later.

---
+++ src/vector/vector.c
@@ -0,0 +1,48 @@
+#include "vector.h"
+
+// Need to be rewritten later.
+// Correct reallocation of a void array.
+void addElement(Vector* vector, void* element) {
+	if (vector->held_data == NULL) {
+		// Just an empty vector dude.
+		vector->held_data = (void* )malloc(sizeof(element));
+		vector->size = 1; 
+	}
+	else if (vector->size >= vector->allocated_capacity) {
+		vector->allocated_capacity *= 2;
+		vector->held_data = (void *)realloc(vector->held_data, vector->allocated_capacity * sizeof(void *));
+	}
+
+	vector->held_data[vector->size] = element;
+	vector->size += 1;
+}
+
+void removeElement(Vector* vector, u_int position) {
+	if (position >= vector->size) {
+		printf("Called index is greater than the vector's size.");
+		exit(1);
+	}
+	vector->held_data[position] = NULL;
+	vector->size -= 1;
+	// could use size though
+	for (size_t i = 0; i < (sizeof(vector->held_data) / sizeof(vector->held_data[0])); ++i) {
+		memmove(&vector->held_data[i - 1], &vector->held_data, vector->held_data[i - 1] * sizeof(void *));
+	}
+}
+
+void* getElement(Vector* vector, u_int position) {
+	if (position >= vector->size) {
+		printf("Called index is greater than the vector's size. Please change the index before calling a function.");
+		exit(1);
+	}
+	return vector->held_data[posiition];
+}
+
+Vector* newVector() {
+	Vector* vector = (Vector* )malloc(sizeof(Vector));
+	vector->held_data = NULL;
+	vector->size = 0;
+	vector->allocated_capacity = 0;
+	return vector;
+}
+
---
+++ src/vector/vector.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef unsigned int u_int;
+
+typedef struct Vector {
+	void** held_data;
+	u_int size;
+	u_int allocated_capacity;
+} Vector;
+
+Vector* newVector();
+void addElement(Vector* vector, void* element);
+void removeElement(Vector* vector, u_int position);
+void* getElement(Vector* vector, u_int position);
